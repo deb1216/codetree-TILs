2b974ff3f75f17dcf8f5f4b462035db147f842e9
deb1216 <duckhu54@gmail.com>
2024-10-01 23:59:25 +0900

    Title: 369 게임, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 241001/369 게임/369-game.java
+++ 241001/369 게임/369-game.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for (int i=1; i<=n; i++) {
-            if (i%10==3 || i%10==6 || i%10==9 || i%3==0)
+            if (i%10==3 || i%10==6 || i%10==9 || i%3==0 || i/10==3 || i/10==6 || i/10==9)
                 System.out.printf("0 ");
             else
                 System.out.printf("%d ", i);
