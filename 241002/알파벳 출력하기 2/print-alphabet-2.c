5ed2b11f39209afc27cde9fbf2e6cf070397c3c0
deb1216 <duckhu54@gmail.com>
2024-10-02 18:12:12 +0900

    Title: 알파벳 출력하기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241002/알파벳 출력하기 2/print-alphabet-2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n);
+    char cnt = 'A';
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<i; j++)
+            printf("  ");
+        for (int j=0; j<n-i; j++) {
+            printf("%c ", cnt);
+            cnt++;
+            if (cnt>'Z')
+                cnt='A';
+        }
+        printf("\n");
+    }
+    return 0;
+}
