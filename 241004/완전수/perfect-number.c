0613614dd634c3fec098a55cd0d4b301d9e447dc
deb1216 <duckhu54@gmail.com>
2024-10-04 13:24:04 +0900

    Title: 완전수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241004/완전수/perfect-number.c
+++ 241004/완전수/perfect-number.c
@@ -2,9 +2,10 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int start, end, cnt=0, sum=0;
+    int start, end, sum, cnt=0;
     scanf("%d %d", &start, &end);
     for (int i=start; i<=end; i++) {
+        sum=0;
         for (int j=1; j<i; j++) {
             if (i%j==0)
                 sum+=j;
