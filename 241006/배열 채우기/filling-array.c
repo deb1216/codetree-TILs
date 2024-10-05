344b719362fa7c6ed15a2556adfbff722ea3a385
deb1216 <duckhu54@gmail.com>
2024-10-06 01:53:03 +0900

    Title: 배열 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241006/배열 채우기/filling-array.c
+++ 241006/배열 채우기/filling-array.c
@@ -2,15 +2,13 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int a[10];
-    for (int i=0; i<10; i++) {
+    int i, a[10];
+    for (i=0; i<10; i++) {
         scanf("%d", &a[i]);
-        if (a[i]==0) {
-            for (int j=i-1; j>=0; j--) {
-                printf("%d ", a[j]);
-            }
+        if (a[i]==0)
             break;
-        }
     }
+    for (int j=i-1; j>=0; j--)
+        printf("%d ", a[j]);
     return 0;
 }
