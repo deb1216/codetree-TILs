8e177869dbf105c1282a432654a594da98ed54c1
deb1216 <duckhu54@gmail.com>
2024-10-08 03:34:28 +0900

    Title: 짝수만 거꾸로 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241008/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.c
+++ 241008/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.c
@@ -4,10 +4,10 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n, i;
     scanf("%d", &n);
-    int a[n];
+    int a[100];
     for (i=0; i<n; i++)
         scanf("%d", &a[i]);
-    for (int j=i; j>=0; j--) {
+    for (int j=i-1; j>=0; j--) {
         if (a[j]%2==0)
             printf("%d ", a[j]);
     }
